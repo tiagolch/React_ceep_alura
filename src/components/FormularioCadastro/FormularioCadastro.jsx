7f0fe24603532191603bdf34041f8df5a1b0c2a9
m2b-tiagochaves <tiagodevch@gmail.com>
2021-08-06 09:01:33 -0300

    criado funçoes no formulario
    Signed-off-by: m2b-tiagochaves <tiagodevch@gmail.com>

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -5,22 +5,38 @@ class FormularioCadastro extends Component {
   constructor(){
     super();
     this.value = "";
+    this.texto = "";
   }
 
-  handleMudancaDeTitulo(evento) {
+  _handleMudancaDeTitulo(evento) {
+    evento.stopPropagation();
     this.value = evento.target.value;
+
+  }
+
+  _handleMudancaDeTexto(evento) {
+    evento.stopPropagation();
+    this.texto = evento.target.value;
+  }
+
+  _criarNota(evento) {
+    evento.preventDefault();
+    evento.stopPropagation();
+    this.props.criarNota(this.value, this.texto);
   }
 
   render() {
     return (
-      <form className="form-cadastro">
+      <form className="form-cadastro"
+      onSubmit={this._criarNota.bind(this)}>
         <input    type="text" 
                   placeholder="Título" 
                   className="form-cadastro_input" 
-                  onChange={this.handleMudancaDeTitulo.bind(this)} />
+                  onChange={this._handleMudancaDeTitulo.bind(this)} />
         <textarea rows={10} 
                   placeholder="Descrição" 
-                  className="form-cadastro_input" />
+                  className="form-cadastro_input" 
+                  onChange={this._handleMudancaDeTexto.bind(this)} />
         <button   type="submit" 
                   className="form-cadastro_input form-cadastro_submit">
         Criar nota
